45d1f3b3582784721edaa230be6fa0061fa26b78
alex-bue <73710596+alex-bue@users.noreply.github.com>
2024-05-21 14:52:17 +0200

    Update keymap.c

--- keyboards/planck/ez/glow/keymaps/alexbue/keymap.c
+++ keyboards/planck/ez/glow/keymaps/alexbue/keymap.c
@@ -28,7 +28,7 @@ enum planck_keycodes {
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 [_QWERTY] = LAYOUT_ortho_4x12(
-    KC_TAB,          KC_W,     KC_W,     KC_E,     KC_R,   KC_T,     KC_Y,     KC_U,   KC_I,     KC_O,     KC_P,     KC_BSPC,
+    KC_TAB,          KC_Q,     KC_W,     KC_E,     KC_R,   KC_T,     KC_Y,     KC_U,   KC_I,     KC_O,     KC_P,     KC_BSPC,
     LCTL_T(KC_ESC),  KC_A,     KC_S,     KC_D,     KC_F,   KC_G,     KC_H,     KC_J,   KC_K,     KC_L,     KC_SCLN,  KC_QUOT,
     KC_LSFT,         KC_Z,     KC_X,     KC_C,     KC_V,   KC_B,     KC_N,     KC_M,   KC_COMM,  KC_DOT,   KC_SLSH,  SC_SENT,
     MO(_NAVIGATION), KC_LCTL,  KC_LALT, KC_LGUI  ,  LT(_LOWER, KC_BSPC),  LT(_NAVIGATION, KC_SPC),  LT(_NAVIGATION, KC_SPC),  LT(_RAISE, KC_ENTER), KC_RGUI, KC_RALT, KC_RCTL, MO(_NAVIGATION)
