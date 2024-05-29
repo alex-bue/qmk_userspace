05a7a07232a45b62c8ea094e37c27b178e16f5af
Alexander Büscher <alibue@hotmail.de>
2024-05-29 18:50:50 +0200

    Finish first attempt on base layer

--- keyboards/planck/ez/glow/keymaps/planck_5x3x3/keymap.c
+++ keyboards/planck/ez/glow/keymaps/planck_5x3x3/keymap.c
@@ -2,27 +2,19 @@
 
 #define LA_SYM MO(_SYM)
 #define LA_NAV MO(_NAV)
-
-// Left-hand home row mods
-#define HOME_A LSFT_T(KC_A)
-#define HOME_S LCTL_T(KC_S)
-#define HOME_D LGUI_T(KC_D)
-#define HOME_F LALT_T(KC_F)
-
-// Right-hand home row mods
-#define HOME_J LALT_T(KC_J)
-#define HOME_K RGUI_T(KC_K)
-#define HOME_L RCTL_T(KC_L)
-#define HOME_SCLN RSFT_T(KC_SCLN)
+#define LA_MEDIA MO(_MEDIA)
+#define LA_NUM MO(_NUM)
+#define LA_SYM MO(_SYM)
 
 // EurKey macros for German Umlaute
 // TODO
 
 enum planck_layers {
     _QWERTY,
-    _SYM,
     _NAV,
+    _MEDIA,
     _NUM,
+    _SYM,
 };
 
 enum planck_keycodes {
@@ -33,32 +25,39 @@ enum planck_keycodes {
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 [_QWERTY] = LAYOUT_ortho_4x12(
-    KC_TAB,          KC_Q,     KC_W,     KC_E,     KC_R,     KC_T,     KC_Y,     KC_U,     KC_I,     KC_O,     KC_P,     KC_BSPC,
-    LCTL_T(KC_ESC),  HOME_A,     HOME_S,     HOME_D,     HOME_F,     KC_G,     KC_H,     HOME_J, HOME_K, HOME_L, HOME_SCLN,   KC_QUOT,
-    KC_LSFT,         KC_Z,     KC_X,     KC_C,     KC_V,     KC_B,     KC_N,     KC_M,     KC_COMM,  KC_DOT,   KC_SLSH,  SC_SENT,
-    _______,         _______,  _______,  KC_LGUI,  LA_NAV,   KC_SPC,   KC_SPC,   LT(LA_SYM, KC_ENTER),   KC_RALT,  _______,  _______,  _______
-),
-
-[_SYM] = LAYOUT_ortho_4x12(
-    _______, KC_EXLM,  KC_AT,    KC_HASH,  KC_DLR,   KC_PERC,  KC_CIRC,  KC_AMPR,  KC_ASTR,  KC_LPRN,  KC_RPRN,  _______,
-    _______, KC_LSFT,  KC_LCTL,  KC_LGUI,  KC_LALT,  _______,  _______,  KC_MINS,  KC_EQL,   KC_LBRC,  KC_RBRC,  KC_BSLS,
-    _______, _______,  _______,  _______,  _______,  _______,  _______,  KC_UNDS,  KC_PLUS,  _______,  _______,  _______,
-    _______, _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______
-),
-
-[_NAV] = LAYOUT_ortho_4x12(
-    _______,  LALT(KC_1),  LALT(KC_2),  LALT(KC_3),  LALT(KC_4),  LALT(KC_5),  KC_MPRV,  KC_VOLD,  KC_VOLU,  KC_MNXT,  KC_MUTE,  _______,
-    _______,  KC_LSFT,     KC_LCTL,     KC_LGUI,     KC_LALT,      _______,     KC_LEFT,  KC_DOWN,  KC_UP,    KC_RGHT,  KC_MPLY,  _______,
-    _______,  _______,     _______,     _______,    _______,     _______,     _______,  KC_BRID,  KC_BRIU,  _______,  _______,  _______,
-    _______,  _______,     _______,     _______,    _______,     _______,     _______,  _______,  _______,  _______,  _______,  QK_BOOT
+    KC_Q,          KC_W,          KC_E,          KC_R,          KC_T,    XXXXXXX,    XXXXXXX,    KC_Y,          KC_U,          KC_I,          KC_O,          KC_P,
+    LCTL_T(KC_A),  LALT_T(KC_S),  LGUI_T(KC_D),  LSFT_T(KC_F),  KC_G,    XXXXXXX,    XXXXXXX,    KC_H,          LSFT_T(KC_J),  LGUI_T(KC_K),  LALT_T(KC_L),  LCTL_T(KC_QUOT),
+    ALGR_T(KC_Z),  KC_X,          KC_C,          KC_V,          KC_B,    XXXXXXX,    XXXXXXX,    KC_N,          KC_M,          KC_COMM,       KC_DOT,       ALGR_T(KC_SLSH),
+    XXXXXXX,       XXXXXXX,       LT(LA_MEDIA, KC_ESC), LT(LA_NAV, KC_TRNS), LT(LA_SYM, KC_TAB), XXXXXXX, XXXXXXX, LT(_NUM, KC_SPC), KC_ENT, KC_BSPC, XXXXXXX, XXXXXXX
 ),
 
 [_NUM] = LAYOUT_ortho_4x12(
-    _______,  KC_1,     KC_2,     KC_3,     KC_4,     KC_5,     KC_6,     KC_7,     KC_8,     KC_9,     KC_0,     _______,
-    _______,  KC_LSFT,  KC_LCTL, KC_LGUI,   KC_LALT,  _______,  _______,  KC_RALT,  KC_RGUI,  KC_RCTL,  KC_RSFT,  _______,
-    _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,
-    _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______
-)
+    KC_LBRC, KC_7,    KC_8,    KC_9,    KC_RBRC, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+    KC_SCLN, KC_4,    KC_5,    KC_6,    KC_EQL,  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+    KC_GRV,  KC_1,    KC_2,    KC_3,    KC_BSLS, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+    XXXXXXX,    XXXXXXX,    XXXXXXX, XXXXXXX, KC_DOT,  XXXXXXX, XXXXXXX, KC_0,    KC_MINS, XXXXXXX, XXXXXXX,    XXXXXXX
+),
+
+[_SYM] = LAYOUT_ortho_4x12(
+    KC_LCBR, KC_AMPR, KC_ASTR, KC_LPRN, KC_RCBR, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+    KC_COLN, KC_DLR,  KC_PERC, KC_CIRC, KC_PLUS, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+    KC_TILD, KC_EXLM, KC_AT,   KC_HASH, KC_PIPE, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+    XXXXXXX,    XXXXXXX,    XXXXXXX, XXXXXXX, KC_LPRN, XXXXXXX, XXXXXXX, KC_RPRN, KC_UNDS, XXXXXXX, XXXXXXX,    XXXXXXX
+),
+
+[_MEDIA] = LAYOUT_ortho_4x12(
+    RGB_TOG, RGB_MOD, RGB_HUI, RGB_SAI, RGB_VAI, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+    KC_MPRV, KC_VOLD, KC_VOLU, KC_MNXT, KC_MPLY, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+    XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+    XXXXXXX,    XXXXXXX,    XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,    XXXXXXX
+),
+
+[_NAV] = LAYOUT_ortho_4x12(
+    KC_HOME, KC_PGDN, KC_PGUP, KC_END,  KC_INS,  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+    KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, CW_TOGG, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+    U_UND,   U_CUT,   U_CPY,   U_PST,   U_RDO,  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+    XXXXXXX,    XXXXXXX,    XXXXXXX, XXXXXXX, KC_DEL,  XXXXXXX, XXXXXXX, KC_BSPC, KC_ENT,  XXXXXXX, XXXXXXX,    XXXXXXX
+),
 
 };
 
