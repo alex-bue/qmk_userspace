02270d181bb4a5fbd63164bd82b2ced814333f67
Alexander Büscher <alibue@hotmail.de>
2024-06-22 01:07:25 +0200

    add miryoku like num layer

--- keyboards/planck/ez/glow/keymaps/alexbue36/keymap.c
+++ keyboards/planck/ez/glow/keymaps/alexbue36/keymap.c
@@ -54,10 +54,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 ),
 
 [_NUM] = LAYOUT_ortho_4x12(
-    KC_1,      KC_2,      KC_3,      KC_4,      KC_5,      XXXXXXX,  XXXXXXX,  KC_6,      KC_7,      KC_8,      KC_9,      KC_0,
-    KC_LCTL,   KC_LALT,    KC_LGUI,    KC_LSFT,   _______,   XXXXXXX,  XXXXXXX,  _______,   KC_LSFT,   KC_LGUI,    KC_LALT,    KC_LCTL,
-    _______,   _______,   _______,   _______,   _______,   XXXXXXX,  XXXXXXX,  _______,   _______,   _______,   _______,   _______,
-    XXXXXXX,   XXXXXXX,   XXXXXXX,   _______,   _______,   XXXXXXX,  XXXXXXX,  _______,   _______,   XXXXXXX,   XXXXXXX,   XXXXXXX
+    KC_LBRC,   KC_7,      KC_8,      KC_9,     KC_RBRC,     XXXXXXX,  XXXXXXX,  _______,   _______,   _______,   _______,   _______,
+    KC_SCLN,   KC_4,      KC_5,      KC_6,     KC_EQL,      XXXXXXX,  XXXXXXX,  _______,   KC_LSFT,   KC_LGUI,    KC_LALT   KC_LCTL,
+    KC_BSLS,   KC_1,      KC_2,      KC_3,     KC_MINS,     XXXXXXX,  XXXXXXX,  _______,   _______,   _______,   _______,   _______,
+    XXXXXXX,   XXXXXXX,   XXXXXXX,   KC_DOT,   KC_0,        XXXXXXX,  XXXXXXX,  _______,   _______,   XXXXXXX,   XXXXXXX,   XXXXXXX
 ),
 
 };
