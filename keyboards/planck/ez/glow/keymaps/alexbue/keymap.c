d629d9ea711881b1c6014e6d37babd2ecbdb58ae
Alexander Büscher <alibue@hotmail.de>
2025-04-16 16:32:21 +0200

    Update sym layer to resemble my zmk keymap and tweaks layer keys...

--- keyboards/planck/ez/glow/keymaps/alexbue/keymap.c
+++ keyboards/planck/ez/glow/keymaps/alexbue/keymap.c
@@ -4,12 +4,14 @@ By Alex
  */
 
 #include QMK_KEYBOARD_H
+#include "g/keymap_combo.h"
 
 enum planck_layers {
   _QWERTY,
   _NAVIGATION,
   _SYM,
   _NUM,
+  _FN,
   _ADJUST
 };
 
@@ -20,47 +22,55 @@ enum planck_keycodes {
   NOCT                  // For Nocture Op 9 1
 };
 
-#define SYM LT(_SYM, KC_ESC)       // Lower layer on hold, backspace on tap
-#define NUM LT(_NUM, KC_ENTER)      // Raise layer on hold, enter on tap
-#define NAVSPC LT(_NAVIGATION, KC_SPC) // Navigation layer on hold, tap on space
-#define NAVTAB LT(_NAVIGATION, KC_TAB) // Alias for navigation layer
+#define SYM LT(_SYM, KC_ENTER)       // Lower layer on hold, backspace on tap
+#define NUM_V LT(_NUM, KC_V)          // Raise layer on hold, enter on tap
+#define NAV_UNDS LT(_NAVIGATION, KC_SPC) // Navigation layer on hold, tap on space
+#define NAV_TAB LT(_NAVIGATION, KC_TAB) // Alias for navigation layer
+#define FN_SPC LT(_FN, KC_SPC) // Alias for navigation layer
 #define NAV MO(_NAVIGATION) // Alias for navigation layer
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 [_QWERTY] = LAYOUT_ortho_4x12(
-    KC_TAB,          KC_Q,     KC_W,     KC_E,     KC_R,   KC_T,     KC_Y,     KC_U,  KC_I,     KC_O,     KC_P,     KC_BSPC,
-    LCTL_T(KC_ESC),  KC_A,     KC_S,     KC_D,     KC_F,   KC_G,     KC_H,     KC_J,  KC_K,     KC_L,     KC_SCLN,  KC_QUOT,
-    KC_LSFT,         KC_Z,     KC_X,     KC_C,     KC_V,   KC_B,     KC_N,     KC_M,  KC_COMM,  KC_DOT,   KC_SLSH,  SC_SENT,
-    NAV, KC_LCTL,    KC_LALT,  KC_LGUI,  SYM,  NAVSPC, NAVSPC,       NUM,  KC_RGUI,  KC_RALT, KC_RCTL, NAV
+    KC_TAB,          KC_Q,     KC_W,     KC_E,      KC_R,     KC_T,     KC_Y,     KC_U,     KC_I,        KC_O,     KC_P,     KC_BSPC,
+    LCTL_T(KC_ESC),  KC_A,     KC_S,     KC_D,      KC_F,     KC_G,     KC_H,     KC_J,     KC_K,        KC_L,     KC_SCLN,  LCTL_T(KC_QUOT),
+    KC_LSFT,         KC_Z,     KC_X,     KC_C,      NUM_V,    KC_B,   KC_N,     KC_M,     KC_COMM,     KC_DOT,   KC_SLSH,  SC_SENT,
+    NAV, KC_LCTL,    KC_LALT,  KC_LGUI,  NAV_UNDS,  FN_SPC,   FN_SPC,   SYM,      KC_RGUI,  KC_RALT,  KC_RCTL,  NAV
 ),
 
 [_NAVIGATION] = LAYOUT_ortho_4x12(
-    _______,  LALT(KC_1),  LALT(KC_2),  LALT(KC_3),  LALT(KC_4),  LALT(KC_5), KC_MPRV,  KC_VOLD,  KC_VOLU,  KC_MNXT,  KC_MUTE,  KC_DEL,
-    _______,  KC_LCTL,     KC_LALT,     KC_LGUI,    KC_LSFT,     _______,     KC_LEFT,  KC_DOWN,  KC_UP,    KC_RGHT,  KC_MPLY,  _______,
-    _______,  _______,     _______,     _______,    _______,     _______,     _______,  KC_BRID, KC_BRIU,  _______,  _______,  _______,
-    _______,  _______,     _______,     _______,    _______,     _______,     _______,  _______,  _______,  _______,  _______,  _______
+    _______,  LALT(KC_1),  LALT(KC_2),  LALT(KC_3),  LALT(KC_4),  LALT(KC_5), KC_HOME,  KC_PGDN,  KC_PGUP,  KC_BSPC,  KC_DEL,  KC_BSPC,
+    _______,  KC_LCTL,     KC_LALT,     KC_LGUI,    KC_LSFT,     _______,     KC_LEFT,  KC_DOWN,  KC_UP,    KC_RGHT,  _______, _______,
+    _______,  _______,     _______,     _______,    _______,     _______,     KC_END,   _______,  _______,  _______,  _______, _______,
+    _______,  _______,     _______,     _______,    _______,     _______,     _______,  _______,  _______,  _______,  _______, _______
 ),
 
 [_SYM] = LAYOUT_ortho_4x12(
-    KC_TILDE, KC_EXLM,  KC_AT,    KC_HASH,  KC_DLR,   KC_PERC,  KC_CIRC,  KC_AMPR,  KC_ASTR,  KC_LPRN,  KC_RPRN,  _______,
-    _______,  KC_LCTL,  KC_LALT,  KC_LGUI,  KC_LSFT,  _______,  _______,  KC_MINS,  KC_EQL,   KC_LBRC,  KC_RBRC,  KC_BSLS,
-    _______,  _______,  _______,  _______,  _______,  _______,  _______,  KC_UNDS,  KC_PLUS,  _______,  _______,  _______,
-    _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______
+    _______,   _______,  LSFT(KC_COMM), LSFT(KC_DOT),  KC_BSLS,    KC_GRV,     KC_AMPR,    KC_GRV,     KC_LPRN,    KC_RPRN,    KC_PERC,   _______,
+    _______,   KC_EXLM,  KC_MINS,       KC_PLUS,       KC_EQL,     KC_HASH,    KC_PIPE,    KC_COLN,    KC_LBRC,    KC_RBRC,    KC_DLR,    _______,
+    _______,   KC_UNDS,  KC_SLSH,       KC_ASTR,       KC_CIRC,    KC_AMPR,    KC_TILD,    KC_SCLN,    KC_LCBR,    KC_RCBR,    KC_AT,     _______,
+    _______,   _______,  _______,       _______,       _______,    _______,    _______,    _______,    _______,    _______,    _______,   _______
 ),
 
 [_NUM] = LAYOUT_ortho_4x12(
-    KC_GRV,   KC_1,     KC_2,     KC_3,     KC_4,     KC_5,     KC_6,     KC_7,     KC_8,     KC_9,     KC_0,     _______,
-    _______,  KC_LCTL,  KC_LALT,  KC_LGUI,  KC_LSFT,  _______,  _______,  KC_LSFT,  KC_LGUI,  KC_LALT,  KC_LCTL,  _______,
-    _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,   _______, _______,  _______,  _______,
-    _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,     _______,  _______,  _______,  _______
+    KC_GRV,   KC_1,     KC_2,     KC_3,     KC_4,     KC_5,     KC_6,     KC_7,     KC_8,     KC_9,     KC_PLUS,  _______,
+    _______,  KC_LCTL,  KC_LALT,  KC_LGUI,  KC_LSFT,  _______,  KC_ASTR,  KC_4,     KC_5,     KC_6,     KC_MINS,  _______,
+    _______,  _______,  _______,  _______,  _______,  _______,  KC_COMM,  KC_1,     KC_2,     KC_3,     KC_DOT,   _______,
+    _______,  _______,  _______,  _______,  _______,  _______,  _______,  KC_0,     _______,  _______,  _______,  _______
+),
+
+[_FN] = LAYOUT_ortho_4x12(
+    _______,   KC_F1,      KC_F2,      KC_F3,      KC_F4,      KC_F5,      KC_TRNS,   KC_MPLY,   KC_MUTE,    KC_TRNS,   KC_TRNS,   _______,
+    _______,   KC_F6,      KC_F7,      KC_F8,      KC_F9,      KC_F10,     KC_MPRV,   KC_VOLD,   KC_VOLU,    KC_MNXT,   KC_TRNS,   _______,
+    _______,   KC_F11,     KC_F12,     KC_F13,     KC_F14,     KC_F15,     KC_TRNS,   KC_BRID,   KC_BRIU,    KC_TRNS,   KC_TRNS,  _______,
+    _______,   _______,   _______,   _______,      KC_TRNS,    KC_TRNS,    KC_TRNS,   KC_TRNS,   _______,   _______,   _______,   _______
 ),
 
 [_ADJUST] = LAYOUT_ortho_4x12(
     _______,  _______,  _______,  _______,  QK_BOOT,  _______,  _______,   _______,  _______,  _______,  AG_TOGG,  RGB_TOG,
     _______,  _______,  _______,  _______,  _______,  _______,  RGB_RMOD,  RGB_VAD,  RGB_VAI,  RGB_MOD,  _______,  AU_TOGG,
     _______,  CAMP,     IMP,      NOCT,     _______,  _______,  _______,   RGB_HUD,  RGB_HUI,  _______,  _______,  _______,
-    _______,  _______,  _______,  _______,  _______,  _______,  _______,   _______,  _______,  _______,  _______,  _______
+    QK_BOOT,  _______,  _______,  _______,  _______,  _______,  _______,   _______,  _______,  _______,  _______,  QK_BOOT
 )
 };
 
@@ -71,7 +81,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 #endif
 
 layer_state_t layer_state_set_user(layer_state_t state) {
-  return update_tri_layer_state(state, _SYM, _NUM, _ADJUST);
+  return update_tri_layer_state(state, _NAVIGATION, _SYM, _ADJUST);
 };
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
@@ -105,7 +115,7 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
 // Adjust tapping term on certain keys
 uint16_t get_tapping_term(uint16_t keycode, keyrecord_t *record) {
     switch (keycode) {
-        case NAVSPC: // Increase time that space needs to be held to access nav layer
+        case FN_SPC: // Increase time that space needs to be held to access nav layer
             return TAPPING_TERM + 150;
         default:
             return TAPPING_TERM;
--- keyboards/planck/ez/glow/keymaps/alexbue/rules.mk
+++ keyboards/planck/ez/glow/keymaps/alexbue/rules.mk
@@ -1,2 +1,4 @@
 SRC += muse.c
+COMBO_ENABLE = yes
+VPATH += keyboards/gboards
 AUDIO_ENABLE = yes
